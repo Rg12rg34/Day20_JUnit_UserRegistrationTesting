66483a5dc286afff8bb4d3f034906504c0315854
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-11 22:32:00 +0530

    Added Junit Test To Test Last Name Format

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -7,7 +7,12 @@ public class UserRegistrationTest {
     UserRegistrationFormat userRegistration = new UserRegistrationFormat();
     @Test
     public void givenName_WhenProper_ShouldReturnTrue(){
-        boolean result = userRegistration.firstName("Rahul");
+        boolean result = userRegistration.firstName("Yogendra");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void giveLastName_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.lastName("Sharma");
         Assert.assertTrue(result);
     }
 }
